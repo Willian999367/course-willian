10ca75c62ea7fe8fabfe405cbb32898675a35e91
Willian999367 <williansiedsch@gmail.com>
2020-06-03 14:46:37 -0300

    Camada de registro, serviço de componentes

--- src/main/java/com/williansiedschlag/course/resources/UserResourse.java
+++ src/main/java/com/williansiedschlag/course/resources/UserResourse.java
@@ -1,21 +1,51 @@
 package com.williansiedschlag.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.williansiedschlag.course.entities.User;
+import com.williansiedschlag.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 
 public class UserResourse {
 	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User (1L,"Willian","willian@com","333-444","12345");
-		return ResponseEntity.ok().body(u);
-	}
+		// Colocar a dependencia do UserService(pacote service)
+		@Autowired 
+		private UserService service; 
 
+		
+		// Função retorna uma lista de usuario List<User>
+		@GetMapping
+		public ResponseEntity<List<User>> findAll(){
+		
+		// Declaro uma var tipo lista
+		// pelo service chamo a função FindAll criada no pacote UserService
+			
+		List<User> list  = service.FindAll();
+		return ResponseEntity.ok().body(list);
+	
+		}
+		
+		// Requisição do tipo Get
+		// Abaixo o get vai poder receber um id 
+		@GetMapping(value = "/{id}") 
+		
+		//Método do tipo User, por que vai retonar apenas um usuario
+		//@PathVariable isso serve para framweork saber que id do GET
+		public ResponseEntity<User> findById(@PathVariable Long id){
+			
+			// Agora chamo a função Find passando o Id do método 
+			User obj = service.findById(id);
+			
+			return ResponseEntity.ok().body(obj);
+		}
+		
 }
---
+++ src/main/java/com/williansiedschlag/course/services/UserService.java
@@ -0,0 +1,37 @@
+package com.williansiedschlag.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.williansiedschlag.course.entities.User;
+import com.williansiedschlag.course.respositories.UserRepository;
+
+//Essa anotacao define como um compenete do spring
+//Com isso vai poder ser injetado automaticamente com autowired
+@Service
+
+public class UserService {
+		
+	@Autowired // Faça a injeção de dependencia
+	private UserRepository repository; 
+
+	// Método para retonar todos usuarios do banco de dados
+	
+	public List<User>FindAll(){
+		return repository.findAll();
+	}
+	
+	// Chamar o user pelo id
+	
+	public User findById(Long id) {
+		
+		
+		Optional<User> obj = repository.findById(id);
+		// Vai retorna um Objeto tipo <User> que estiver dentro o obg 
+		return obj.get();
+	}	
+	
+}

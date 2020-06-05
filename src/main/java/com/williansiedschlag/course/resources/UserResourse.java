64bd8c87bdb0bb2e8d6960a63a1071908796a2f7
Willian999367 <williansiedsch@gmail.com>
2020-06-05 15:34:25 -0300

    delete

--- src/main/java/com/williansiedschlag/course/resources/UserResourse.java
+++ src/main/java/com/williansiedschlag/course/resources/UserResourse.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,4 +67,13 @@ public class UserResourse {
 			return ResponseEntity.created(uri).body(obj); 
 			
 		}
+		
+		
+		// Para o long id ser considerado uma variavel da url @PathVariable
+		@DeleteMapping(value = "/{id}") 
+		public ResponseEntity<Void> delete(@PathVariable Long id){
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		}
+		
 }

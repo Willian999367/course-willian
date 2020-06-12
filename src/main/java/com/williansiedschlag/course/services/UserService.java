a4755f4096b9c4342456f7a8d2ab30f984737288
Willian999367 <williansiedsch@gmail.com>
2020-06-12 15:29:16 -0300

    tratamento de exceção

--- src/main/java/com/williansiedschlag/course/services/UserService.java
+++ src/main/java/com/williansiedschlag/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.williansiedschlag.course.entities.User;
 import com.williansiedschlag.course.respositories.UserRepository;
+import com.williansiedschlag.course.services.exceptions.DatabaseException;
 import com.williansiedschlag.course.services.exceptions.ResourceNotFoundException;
 
 //Essa anotacao define como um compenete do spring
@@ -38,9 +41,19 @@ public class UserService {
 		return repository.save(obj);
 	}
 	
+	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
+	
+	
+	
 	
 	public User update(Long id, User obj) {
 		
